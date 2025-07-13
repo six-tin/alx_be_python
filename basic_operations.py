7a89cb3a51cd8735bf4ae8d9d27fab79667e5379
six-tin <Ayomidejohnson988@gmail.com>
2025-07-13 11:11:02 -0700

    Add basic arithmetic operations script

--- basic_operations.py
+++ basic_operations.py
@@ -1,12 +1,6 @@
 number1 = 10
 number2 = 5
 
-# Perform operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
-
-# Print results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
